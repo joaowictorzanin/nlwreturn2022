ea3ead2fbf1fe1dc0043b362a8c3c080c2ad7770
joao.zanin <103003839+joaowictorzanin@users.noreply.github.com>
2022-05-04 20:13:11 -0300

    Dia 02

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,13 +1,22 @@
+const { borderRadius } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257E5'
-        }
-      }
+        },
+      },
+      borderRadius: {
+          md:'4px'
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
